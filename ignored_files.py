83232aa7686c9daf0d1309fff3f82711b80c59c1
e-caste-mbp <en.cas.dev@icloud.com>
2020-10-06 11:25:45 +0200

    update list of files to ignore

--- ignored_files.py
+++ ignored_files.py
@@ -1,6 +1,14 @@
 ignored_files = [
     "*.txt",
     "*.md",
+    "*.xml",
     "LICENSE",
-    ".gitignore"
+    ".git*",
+    "*.min.js",
+    "*.min.css",
+    "*.svg",
+    "composer.json",
+    "composer.lock",
+    "Pipfile",
+    "Pipfile.lock",
 ]
