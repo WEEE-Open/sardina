454f484e447ccb12ffc97c703b029ad5a61ec416
e-caste-mbp <en.cas.dev@icloud.com>
2020-10-08 12:11:42 +0200

    add get_contributors_commits_stats function to rank contributors

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 from subprocess import run
 
 from ignored_files import ignored_files
@@ -52,6 +52,43 @@ def get_anonymous_commits_stats(repos: list) -> dict:
               "re-run this script in >10 minutes to get their stats as well...")
         for repo in [r for r in repos if r not in already_checked_repos]:
             requests.get(f"{url_api}/repos/{owner}/{repo}/stats/commit_activity")
+
+    print("\n")
+    return stats
+
+
+def get_contributors_commits_stats(repos: list) -> dict:
+    # see https://docs.github.com/en/free-pro-team@latest/rest/reference/repos#get-all-contributor-commit-activity
+    stats = {'total': {}, 'past_year': {}}
+    unix_one_year_ago = int((datetime.now() - timedelta(days=365)).timestamp())
+
+    for repo in repos:
+        response = requests.get(f"{url_api}/repos/{owner}/{repo}/stats/contributors")
+        print(f"{repo} gave response {response.status_code}")
+
+        if response.status_code == 403:
+            raise_rate_limited_exception()
+
+        elif response.status_code == 200:
+            json = response.json()
+            stats[repo] = {
+                'total': {author['author']['login']: author['total']
+                          for author in json},
+                'past_year': {author['author']['login']: sum(week['c']
+                                                             for week in author['weeks']
+                                                             if week['w'] > unix_one_year_ago)
+                              for author in json},
+            }
+
+            for author in json:
+                login = author['author']['login']
+                if login not in stats['total']:
+                    stats['total'][login] = 0
+                    stats['past_year'][login] = 0
+                stats['total'][login] += author['total']
+                stats['past_year'][login] += sum(week['c']
+                                                 for week in author['weeks']
+                                                 if week['w'] > unix_one_year_ago)
 
     print("\n")
     return stats
@@ -150,6 +187,7 @@ def main():
 
     repos = get_repos()
     commits_stats = get_anonymous_commits_stats(repos) if get_commits else None
+    contributors_stats = get_contributors_commits_stats(repos) if get_commits else None
     lines_stats = get_lines_stats(repos, use_cloc)
     print_all_stats(commits_stats, lines_stats, use_cloc)
 
