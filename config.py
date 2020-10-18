ecf10d6c80a5ee2fd35539ed3e2ac9dde5e27dd2
alecello <alessandro.celoria@hotmail.com>
2020-10-18 12:48:02 +0200

    Added configuration file config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# Set this to true if you want to cache requests locally for fast development
+devmode = True
+
+# Set this to true if you also want to retain *all* cloned repositories. Mind the storage space!
+keep_repos = True
