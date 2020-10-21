7a2c4d6d528cd85508e161ca7d94d0ef21e53789
e-caste-mbp <en.cas.dev@icloud.com>
2020-10-21 11:08:35 +0200

    move token from github_pat.py to config.py

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ owner = "WEEE-Open"
 is_organization = True  # True for multi-contributor organizations, False for single users
 output_file = "stats"
 output_dir = "output"
+# your PAT generated at https://github.com/settings/tokens - see README
+token = "YOUR TOKEN HERE"
 
 # Set this to true if you want to cache requests locally for fast development
 dev_mode = False
--- github_pat.py
+++
@@ -1,2 +0,0 @@
-# your PAT generated at https://github.com/settings/tokens - see README
-token = "YOUR TOKEN HERE"
