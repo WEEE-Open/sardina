de4fae3a5fc332856a5ae180053c7f6792ae3093
alecello <alessandro.celoria@hotmail.com>
2020-10-24 19:43:41 +0200

    Removed another outdated todo

--- main.py
+++ main.py
@@ -465,6 +465,7 @@ def print_all_stats(commits_stats: dict, lines_stats: dict, contributors_stats: 
                         sloc_by_repo[repo] = Graph(dict(lines_stats[repo]), 1, 1, 'pie', 'Type', f'Line distribution for repository {owner}/{repo}')
 
             else:
+                minimum = lines_stats['total'] * 0.005
                 total_sloc = Graph(dict(lines_stats), minimum, 1, 'pie', 'Repository', 'SLOC count by repository')
                 global_graphs['sloc.svg'] = total_sloc
 
@@ -475,8 +476,6 @@ def print_all_stats(commits_stats: dict, lines_stats: dict, contributors_stats: 
             generate_figure([global_graphs[graph]], os.path.join(graph_dir, owner, graph))
 
         generate_figure(global_graphs.values(), os.path.join(graph_dir, owner, 'combined.svg'))
-
-        # TODO: Generate all the other graphs
 
     if commits_stats is not None:
         commits_output = "\n".join([f"{repo}: {commits_stats[repo]} commits past year"
