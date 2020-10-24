347dbac99f3cf61ad156fc17c056521cca5dab41
alecello <alessandro.celoria@hotmail.com>
2020-10-24 11:11:33 +0200

    Removed now useless warning

--- ignored_files.py
+++ ignored_files.py
@@ -1,12 +1,4 @@
 # These are Python regular expressions, treat them as such
-# WARNING: Do not use the start anchor (^) in your expressions
-#          as the path that these expressions are evaluated against
-#          is a relative path to the script directory,
-#          so they will all be in the form:
-#
-#          repos/<repo_name>/file/path/
-#
-#          This is by design since cloc just works this way.
 ignored_files = [
     "^.*\.txt$",
     "^.*\.md$",
